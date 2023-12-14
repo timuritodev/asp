41dff74feacfd83aec6ea2fa9b0c08ceb783e1d9
luciD <sosalnet@yandex.ru>
2023-12-14 14:36:12 +0300

    added images

--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -1,11 +1,13 @@
 namespace ASP.Models
 {
     public class Product
-    {
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int InStock { get; set; }
-    }
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int InStock { get; set; }
+    public string ImageUrl { get; set; } // Добавьте это свойство
+}
+
 }
